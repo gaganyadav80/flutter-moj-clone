4e2dca7ae090a9bfd01d4df0d11e18ae67729177
mrmayurgithub <mr.mayurrrr@gmail.com>
2021-02-12 16:32:30 +0530

    update

---
+++ lib/utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+double screenHeight;
+
+double screenwidth;
+
+void initializeUtils(BuildContext context) {
+  final _size = MediaQuery.of(context).size;
+  screenHeight = _size.height;
+  screenwidth = _size.width;
+}
