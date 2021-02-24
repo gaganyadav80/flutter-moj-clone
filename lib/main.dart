4ac4eed2e46cf2bc31d09942644b67d487e85443
Gagan <gaganyadav80@gmail.com>
2021-02-24 16:03:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:chewie/chewie.dart';
 import 'package:edverhub_video_editor/ui/pages/camera_screen.dart';
